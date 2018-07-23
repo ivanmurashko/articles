61957fb99f972c253bc08f0742f4a41b2d4c9db0
Ivan Murashko <ivan.murashko@gmail.com>
2018-07-23 15:37:59 +0300

    cattheory: cpp code update

--- cattheory/src/cpp/category.cpp
+++ cattheory/src/cpp/category.cpp
@@ -1,5 +1,5 @@
-#include <iostream>
 #include <functional>
+#include <iostream>
 #include <string>
 
 auto isEven = [](int x) { return x % 2 == 0; };
@@ -7,16 +7,13 @@ auto isEven = [](int x) { return x % 2 == 0; };
 auto stringLength = [](std::string s) { return static_cast<int>(s.size()); };
 
 // h = g . f
-template < typename A, typename B> 
-auto compose(A g, B f) 
-{
-  auto h = [f,g](auto a)
-         {
-           auto b = f(a);
-           auto c = g(b);
-           return c;           
-         };  
-  return h;  
+template <typename A, typename B> auto compose(A g, B f) {
+  auto h = [f, g](auto a) {
+    auto b = f(a);
+    auto c = g(b);
+    return c;
+  };
+  return h;
 };
 
 auto isStringLengthEven = compose<>(isEven, stringLength);
@@ -27,17 +24,15 @@ auto isStringLengthEvenLeft = compose<>(id, isStringLengthEven);
 
 auto isStringLengthEvenRight = compose<>(isStringLengthEven, id);
 
+int main() {
+  std::cout << "3: " << isStringLengthEven("abc") << std::endl;
+  std::cout << "4: " << isStringLengthEven("abcd") << std::endl;
 
-int main()
-{
-  std::cout << "3: " << isStringLengthEven("abc") << std::endl;  
-  std::cout << "4: " << isStringLengthEven("abcd") << std::endl;  
+  std::cout << "id . 3: " << isStringLengthEvenLeft("abc") << std::endl;
+  std::cout << "id . 4: " << isStringLengthEvenLeft("abcd") << std::endl;
 
-  std::cout << "id . 3: " << isStringLengthEvenLeft("abc") << std::endl;  
-  std::cout << "id . 4: " << isStringLengthEvenLeft("abcd") << std::endl;  
-
-  std::cout << "3 . id: " << isStringLengthEvenRight("abc") << std::endl;  
-  std::cout << "4 . id: " << isStringLengthEvenRight("abcd") << std::endl;  
+  std::cout << "3 . id: " << isStringLengthEvenRight("abc") << std::endl;
+  std::cout << "4 . id: " << isStringLengthEvenRight("abcd") << std::endl;
 
   return 0;
 }
