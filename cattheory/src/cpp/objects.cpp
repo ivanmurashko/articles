f2ea73cc9444d25a632f03daf240fc7f94ee79b2
Ivan Murashko <ivan.murashko@gmail.com>
2018-08-05 00:18:19 +0300

    cattheory: c++ examples for product and sum

--- cattheory/src/cpp/objects.cpp
+++ cattheory/src/cpp/objects.cpp
@@ -1,5 +1,31 @@
 #include <utility>
 #include <iostream>
+#include <string>
+#include <variant>
+
+template <typename A, typename B> union Either
+{
+  A left;
+  B right;  
+};
+
+template <typename A, typename B, typename C, typename D>
+auto factor(A f, B g) {
+  auto h = [f,g](std::variant<C, D> either) {
+             C* left = std::get_if<C>(&either);
+             if (left){
+               return f(left);               
+             }              
+             D* right = std::get_if<D>(&either);
+             return g(right);             
+           };
+  return h;
+};
+
+auto stringLength = [](std::string s) { return static_cast<int>(s.size()); };
+auto id = [](auto x) { return x; };
+
+
 
 int main()
 {
@@ -8,5 +34,14 @@ int main()
   std::cout << "First projector: " << data.first << std::endl;
   std::cout << "Second projector: " << data.second << std::endl;
 
+  std::variant<std::string, int> v;
+  v = "abc";
+  std::cout << "String length:" <<
+    factor(stringLength, id)(v) << std::endl;
+  v = 4;
+  std::cout << "id(int):" <<
+    factor(stringLength, id)(v) << std::endl;
+  
+  
   return 0;
 }
