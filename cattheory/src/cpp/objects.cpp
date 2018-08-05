1bf10a4e2171a89dada9244eefa27d7d3ec6f844
Ivan Murashko <ivan.murashko@gmail.com>
2018-08-05 17:24:11 +0300

    cattheory: c++ examples for product and sum

--- cattheory/src/cpp/objects.cpp
+++ cattheory/src/cpp/objects.cpp
@@ -1,31 +1,45 @@
 #include <utility>
 #include <iostream>
 #include <string>
-#include <variant>
+#include <stdexcept>
 
-template <typename A, typename B> union Either
+template <typename A, typename B> class Either
 {
-  A left;
-  B right;  
+public:
+  Either(const A& l) : left_(l), is_left_(true){    
+  }
+  Either(const B& r) : right_(r), is_left_(false){    
+  }
+  const A& left() const {
+    if (!is_left_){
+      throw std::logic_error("no left");      
+    }    
+    return left_;    
+  }
+  const B& right() const {
+    if (is_left_){
+      throw std::logic_error("no right");      
+    }    
+    return right_;    
+  }
+private:
+  A left_;
+  B right_;
+  bool is_left_;  
 };
 
 template <typename A, typename B, typename C, typename D>
-auto factor(A f, B g) {
-  auto h = [f,g](std::variant<C, D> either) {
-             C* left = std::get_if<C>(&either);
-             if (left){
-               return f(left);               
-             }              
-             D* right = std::get_if<D>(&either);
-             return g(right);             
-           };
-  return h;
+auto factor(A f, B g, const Either<C, D>& either) {
+  try {
+    return f(either.left());               
+  }
+  catch(...) {
+    return g(either.right());                              
+  }
 };
 
 auto stringLength = [](std::string s) { return static_cast<int>(s.size()); };
 auto id = [](auto x) { return x; };
-
-
 
 int main()
 {
@@ -34,13 +48,12 @@ int main()
   std::cout << "First projector: " << data.first << std::endl;
   std::cout << "Second projector: " << data.second << std::endl;
 
-  std::variant<std::string, int> v;
-  v = "abc";
+  Either<std::string, int> str = std::string("abc");
   std::cout << "String length:" <<
-    factor(stringLength, id)(v) << std::endl;
-  v = 4;
+    factor<>(stringLength, id, str) << std::endl;
+  Either<std::string, int> i = 4;
   std::cout << "id(int):" <<
-    factor(stringLength, id)(v) << std::endl;
+    factor<>(stringLength, id, i) << std::endl;
   
   
   return 0;
