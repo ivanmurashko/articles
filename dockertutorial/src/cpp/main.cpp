6442cdb9316602c4dbfd0ceac70b3a4049240a5a
Ivan Murashko <ivan.murashko@gmail.com>
2019-10-03 17:35:53 +0300

    docker: addons

--- dockertutorial/src/cpp/main.cpp
+++ dockertutorial/src/cpp/main.cpp
@@ -1,16 +1,16 @@
 #include "Time.h"
-#include <ctime>
-#include <string>
 #include <thrift/protocol/TBinaryProtocol.h>
 #include <thrift/server/TSimpleServer.h>
 #include <thrift/transport/TBufferTransports.h>
 #include <thrift/transport/TServerSocket.h>
+#include <string>
+#include <ctime>
 
 using namespace ::apache::thrift;
 using namespace ::apache::thrift::protocol;
 using namespace ::apache::thrift::transport;
 using namespace ::apache::thrift::server;
-using namespace ::apache::thrift::stdcxx::shared_ptr;
+
 using namespace ::proto;
 
 class TimeHandler : virtual public TimeIf {
@@ -29,14 +29,19 @@ public:
 
 int main(int argc, char **argv) {
   int port = 9191;
-  shared_ptr<TimeHandler> handler(new TimeHandler());
-  shared_ptr<TProcessor> processor(new TimeProcessor(handler));
-  shared_ptr<TServerTransport> serverTransport(new TServerSocket(port));
-  shared_ptr<TTransportFactory> transportFactory(
-      new TBufferedTransportFactory());
-  shared_ptr<TProtocolFactory> protocolFactory(new TBinaryProtocolFactory());
+  ::apache::thrift::stdcxx::shared_ptr<TimeHandler> 
+    handler(new TimeHandler());
+  ::apache::thrift::stdcxx::shared_ptr<TProcessor> 
+    processor(new TimeProcessor(handler));
+  ::apache::thrift::stdcxx::shared_ptr<TServerTransport> 
+    serverTransport(new TServerSocket(port));
+  ::apache::thrift::stdcxx::shared_ptr<TTransportFactory> 
+    transportFactory(new TBufferedTransportFactory());
+  ::apache::thrift::stdcxx::shared_ptr<TProtocolFactory> 
+    protocolFactory(new TBinaryProtocolFactory());
 
-  TSimpleServer server(processor, serverTransport, transportFactory,
+  TSimpleServer server(processor, serverTransport, 
+                       transportFactory,
                        protocolFactory);
   server.serve();
   return 0;
